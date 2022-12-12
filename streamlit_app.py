b9082b2a61672b616fd76d3b97807c9257fa963e
TomJohn <115263325+TomJohnH@users.noreply.github.com>
2022-12-12 18:43:06 +0100

    Add files via upload
    code refactor, css changes in order to improve visuals

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ import time
 import pandas as pd
 from random import randrange
 
+# ---------------- callbacks ----------------
+
 
 def left_callback():
     st.session_state.left_clicked = True
@@ -22,7 +24,9 @@ def down_callback():
     st.session_state.down_clicked = True
 
 
-# getting level
+# ---------------- function - load data with level ----------------
+
+# level is an array - please check level renderer for more details
 
 
 @st.cache
@@ -31,39 +35,31 @@ def fetch_data():
     return df
 
 
-# external css
+# ---------------- function - external css definition ----------------
+
+
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-# level renderer
+# ---------------- function & data definition - level renderer ----------------
+
+# level is an array with values specified below
+# each value of matrix is rendered into html value
 
 tileset = {
     "E": "&#xB7;",  # "&#xA0;",
     "W": '<span style="color:White">&#x2593;</span>',
     "C": "&#x2591;",
     "D": '<span style="color:Grey">D</span>',
-    "@": '<span style="color:green">@</span>',
+    "@": '<span style="color:GreenYellow">@</span>',
     "G": '<span style="color:gold">G</span>',
     "M": '<span style="color:white">M</span>',
     "B": '<span style="color:red">B</span>',
     "N": "&#xA0;",
+    "X": '<span style="color:violet">X</span>',
 }
-tileset_movable = {
-    "E": True,
-    "W": False,
-    "C": True,
-    "D": True,
-    "@": False,
-    "G": True,
-    "M": True,
-    "B": True,
-    "N": False,
-}
-
-
-move_directions = ["l", "r", "u", "d"]
 
 
 def level_renderer(df):
@@ -77,13 +73,32 @@ def level_renderer(df):
     return l1
 
 
-# array_mover
+# ---------------- function & data - character mover ----------------
+
+# tileset_movable - dictionary of bools for collision detection
+# inital_level - state of inital level array
+# level_temp - state of output level array
+
+tileset_movable = {
+    "E": True,
+    "W": False,
+    "C": True,
+    "D": True,
+    "@": False,
+    "G": True,
+    "M": True,
+    "B": True,
+    "N": False,
+    "X": True,
+}
+
+move_directions = ["l", "r", "u", "d"]
 
 
-def move(inital_array, direction, who):
+def move(inital_level, direction, who):
 
     # where is the player?
-    k = np.where(inital_array == who)
+    k = np.where(inital_level == who)
 
     # check if we have anything to return (important for non-player)
     if any(k[0]):
@@ -92,67 +107,67 @@ def move(inital_array, direction, who):
         # st.write(k)
 
         # let's copy input array and remove player
-        array_temp = inital_array.copy()
-        array_temp[from_top, from_left] = "E"
+        level_temp = inital_level.copy()
+        level_temp[from_top, from_left] = "E"
 
         # let's check where player can move
         movement = {}
-        movement["lt"] = inital_array[from_top, from_left - 1]  # left
-        movement["rt"] = inital_array[from_top, from_left + 1]  # right
-        movement["ut"] = inital_array[from_top - 1, from_left]  # up
-        movement["dt"] = inital_array[from_top + 1, from_left]  # down
-        st.session_state["previous_movement_state"] = movement
+        movement["lt"] = inital_level[from_top, from_left - 1]  # left
+        movement["rt"] = inital_level[from_top, from_left + 1]  # right
+        movement["ut"] = inital_level[from_top - 1, from_left]  # up
+        movement["dt"] = inital_level[from_top + 1, from_left]  # down
+        # st.session_state["previous_movement_state"] = movement
 
         # st.write(movement)
 
         if direction == "l":
             if from_left > 0:
                 if tileset_movable[movement["lt"]] == True:
-                    array_temp[from_top, from_left - 1] = who
-                    st.session_state["previous_move"] = "l"
-                    return array_temp
+                    level_temp[from_top, from_left - 1] = who
+                    # st.session_state["previous_move"] = "l"
+                    return level_temp
                 else:
-                    return inital_array
+                    return inital_level
             else:
-                return inital_array
+                return inital_level
 
         if direction == "r":
             if from_left < 48:
                 # st.write(from_left)
                 if tileset_movable[movement["rt"]] == True:
-                    array_temp[from_top, from_left + 1] = who
-                    st.session_state["previous_move"] = "r"
-                    return array_temp
+                    level_temp[from_top, from_left + 1] = who
+                    # st.session_state["previous_move"] = "r"
+                    return level_temp
                 else:
-                    return inital_array
+                    return inital_level
             else:
-                return inital_array
+                return inital_level
         if direction == "u":
             if from_top > 0:
                 if tileset_movable[movement["ut"]] == True:
-                    array_temp[from_top - 1, from_left] = who
-                    st.session_state["previous_move"] = "u"
-                    return array_temp
+                    level_temp[from_top - 1, from_left] = who
+                    # st.session_state["previous_move"] = "u"
+                    return level_temp
                 else:
-                    return inital_array
+                    return inital_level
             else:
-                return inital_array
+                return inital_level
 
         if direction == "d":
             if from_top < 48:
                 if tileset_movable[movement["dt"]] == True:
-                    array_temp[from_top + 1, from_left] = who
-                    st.session_state["previous_move"] = "d"
-                    return array_temp
+                    level_temp[from_top + 1, from_left] = who
+                    # st.session_state["previous_move"] = "d"
+                    return level_temp
                 else:
-                    return inital_array
+                    return inital_level
             else:
-                return inital_array
+                return inital_level
     else:
-        return inital_array
+        return inital_level
 
 
-# initiate buttons & variables
+# ---------------- initiat session states ----------------
 
 if "left_clicked" not in st.session_state:
     st.session_state["left_clicked"] = False
@@ -166,14 +181,26 @@ if "up_clicked" not in st.session_state:
 if "down_clicked" not in st.session_state:
     st.session_state["down_clicked"] = False
 
-if "gold" not in st.session_state:
-    st.session_state["gold"] = 0
+if "backpack" not in st.session_state:
+    st.session_state["backpack"] = {"gold": 0}
 
-#################################
+if "hero_stats" not in st.session_state:
+    st.session_state["hero_stats"] = {
+        "hp": 20,
+        "max_hp": 20,
+        "exp": 0,
+        "lvl_exp": 10,
+        "kills": 0,
+    }
+
+if "ending_condition" not in st.session_state:
+    st.session_state["ending_condition"] = False
+
+#######################################################
 #
-#       MAIN GAME CODE
+#                   MAIN GAME CODE
 #
-#################################
+#######################################################
 
 # ---------------- CSS ----------------
 
@@ -183,26 +210,32 @@ local_css("style.css")
 
 df = fetch_data()
 
-if "array" not in st.session_state:
-    st.session_state["array"] = df.values
+if "level" not in st.session_state:
+    st.session_state["level"] = df.values
 
-if "array_before_move" not in st.session_state:
-    st.session_state["array_before_move"] = df.values
+if "level_before_move" not in st.session_state:
+    st.session_state["level_before_move"] = df.values
+
+# ---------------- graphic engine pt 1 :-) ----------------
 
 display_html = st.empty()
-html = level_renderer(st.session_state["array"])  # chamber_renderer(size)
-display_html = st.markdown(html, unsafe_allow_html=True)
+html = level_renderer(st.session_state["level"])  # chamber_renderer(size)
 
-#############################
+if not st.session_state["ending_condition"]:
+    display_html.markdown(html, unsafe_allow_html=True)
+
+#######################################################
 #
-#   game objects interactions
+#                   REACTIONS TO BUTTONS
 #
-##########################
+#######################################################
+
+# ---------------- function - check if player is interacting with something ----------------
 
 
 def Interaction(game_object):
     # actual position of the player
-    k = np.where(st.session_state["array"] == "@")
+    k = np.where(st.session_state["level"] == "@")
     # st.write(k)
     from_top = k[0][0]
     from_left = k[1][0]
@@ -212,175 +245,148 @@ def Interaction(game_object):
 
     # check if actual position of player is equal to position of Door
     for i in range(
-        len(np.where(st.session_state["array_before_move"] == game_object)[0])
+        len(np.where(st.session_state["level_before_move"] == game_object)[0])
     ):
 
         if (
             from_top
-            == np.where(st.session_state["array_before_move"] == game_object)[0][i]
+            == np.where(st.session_state["level_before_move"] == game_object)[0][i]
             and from_left
-            == np.where(st.session_state["array_before_move"] == game_object)[1][i]
+            == np.where(st.session_state["level_before_move"] == game_object)[1][i]
         ):
             result = True
 
     return result
 
 
-#################################
-#
-#       REACTIONS TO BUTTONS
-#
-#################################
+# ---------------- if button or key pressed move player ----------------
 
-if st.session_state.left_clicked:
+if st.session_state.left_clicked and not st.session_state["ending_condition"]:
     # status = st.write(move(df.values, "left"))  # st.write("left")
 
-    st.session_state["array"] = move(st.session_state["array"], "l", "@").copy()
-    # move boss
-    st.session_state["array"] = move(
-        st.session_state["array"], move_directions[randrange(4)], "B"
-    ).copy()
-    # move monster
-    st.session_state["array"] = move(
-        st.session_state["array"], move_directions[randrange(4)], "M"
+    st.session_state["level"] = move(st.session_state["level"], "l", "@").copy()
+
+if st.session_state.right_clicked and not st.session_state["ending_condition"]:
+    # status = st.write(move(df.values, "left"))  # st.write("left")
+
+    st.session_state["level"] = move(st.session_state["level"], "r", "@").copy()
+
+if st.session_state.up_clicked and not st.session_state["ending_condition"]:
+    # status = st.write(move(df.values, "left"))  # st.write("left")
+
+    st.session_state["level"] = move(st.session_state["level"], "u", "@").copy()
+
+if st.session_state.down_clicked and not st.session_state["ending_condition"]:
+    # status = st.write(move(df.values, "left"))  # st.write("left")
+
+    st.session_state["level"] = move(st.session_state["level"], "d", "@").copy()
+
+# ---------------- if button or key pressed interact and render graphics ----------------
+
+if (
+    st.session_state.left_clicked
+    or st.session_state.right_clicked
+    or st.session_state.up_clicked
+    or st.session_state.down_clicked
+) and not st.session_state["ending_condition"]:
+
+    # ---------------- move boss ----------------
+
+    st.session_state["level"] = move(
+        st.session_state["level"], move_directions[randrange(4)], "B"
     ).copy()
 
-    html = level_renderer(st.session_state["array"])
+    # ---------------- move monster ----------------
+
+    st.session_state["level"] = move(
+        st.session_state["level"], move_directions[randrange(4)], "M"
+    ).copy()
+
+    # ---------------- graphic engine pt  2:-) ----------------
+
+    html = level_renderer(st.session_state["level"])
+
+    display_html.empty()
+    display_html = st.markdown(html, unsafe_allow_html=True)
+
+    if Interaction("D") == True:
+        # st.write(Door())
+        st.write("door opened")
+
+    if Interaction("G") == True:
+        # st.write(Door())
+        st.write("gold acquired")
+        st.session_state["backpack"]["gold"] = st.session_state["backpack"]["gold"] + 10
+
+    if Interaction("M") == True:
+        damage = randrange(st.session_state["hero_stats"]["max_hp"]) + 100
+        st.write("fight with monster")
+        st.session_state["hero_stats"]["hp"] = (
+            st.session_state["hero_stats"]["hp"] - damage
+        )
+        st.write("-" + str(damage) + " hp")
+        st.session_state["hero_stats"]["kills"] = (
+            st.session_state["hero_stats"]["kills"] + 1
+        )
+
+    if Interaction("B") == True:
+        damage = randrange(st.session_state["hero_stats"]["max_hp"])
+        st.write("fight with boss")
+        st.session_state["hero_stats"]["hp"] = (
+            st.session_state["hero_stats"]["hp"] - damage
+        )
+        st.write("-" + str(damage) + " hp")
+        st.session_state["hero_stats"]["kills"] = (
+            st.session_state["hero_stats"]["kills"] + 1
+        )
+
+    if st.session_state["hero_stats"]["hp"] <= 0:
+        st.session_state["ending_condition"] = True
+
+    if Interaction("X") == True:
+        st.session_state["ending_condition"] = True
+
+    # ---------------- update new inital state of level ----------------
+
+    st.session_state["level_before_move"] = st.session_state["level"]
+
+    # ---------------- reset buttons state ----------------
+
     st.session_state.left_clicked = False
-    display_html.empty()
-    display_html = st.markdown(html, unsafe_allow_html=True)
-
-    #
-    # WRAP THIS PART LATER
-    #
-
-    if Interaction("D") == True:
-        # st.write(Door())
-        st.write("door opened")
-
-    if Interaction("G") == True:
-        # st.write(Door())
-        st.write("gold acquired")
-        st.session_state["gold"] = st.session_state["gold"] + 10
-
-    # update map
-    st.session_state["array_before_move"] = st.session_state["array"]
-
-if st.session_state.right_clicked:
-    # status = st.write(move(df.values, "left"))  # st.write("left")
-    st.session_state["array"] = move(st.session_state["array"], "r", "@").copy()
-    # move boss
-    st.session_state["array"] = move(
-        st.session_state["array"], move_directions[randrange(4)], "B"
-    ).copy()
-    # move monster
-    st.session_state["array"] = move(
-        st.session_state["array"], move_directions[randrange(4)], "M"
-    ).copy()
-    html = level_renderer(st.session_state["array"])
-    st.session_state.right_clicked = False
-    display_html.empty()
-    display_html = st.markdown(html, unsafe_allow_html=True)
-
-    #
-    # WRAP THIS PART LATER
-    #
-
-    if Interaction("D") == True:
-        # st.write(Door())
-        st.write("door opened")
-
-    if Interaction("G") == True:
-        # st.write(Door())
-        st.write("gold acquired")
-        st.session_state["gold"] = st.session_state["gold"] + 10
-
-    # update map
-    st.session_state["array_before_move"] = st.session_state["array"]
-
-if st.session_state.up_clicked:
-    # status = st.write(move(df.values, "left"))  # st.write("left")
-    st.session_state["array"] = move(st.session_state["array"], "u", "@").copy()
-    # move boss
-    st.session_state["array"] = move(
-        st.session_state["array"], move_directions[randrange(4)], "B"
-    ).copy()
-    # move monster
-    st.session_state["array"] = move(
-        st.session_state["array"], move_directions[randrange(4)], "M"
-    ).copy()
-    html = level_renderer(st.session_state["array"])
     st.session_state.up_clicked = False
-    display_html.empty()
-    display_html = st.markdown(html, unsafe_allow_html=True)
-
-    #
-    # WRAP THIS PART LATER
-    #
-
-    if Interaction("D") == True:
-        # st.write(Door())
-        st.write("door opened")
-
-    if Interaction("G") == True:
-        # st.write(Door())
-        st.write("gold acquired")
-        st.session_state["gold"] = st.session_state["gold"] + 10
-
-    # update map
-    st.session_state["array_before_move"] = st.session_state["array"]
-
-if st.session_state.down_clicked:
-    # status = st.write(move(df.values, "left"))  # st.write("left")
-
-    # move player
-    st.session_state["array"] = move(st.session_state["array"], "d", "@").copy()
-
-    # move boss
-    st.session_state["array"] = move(
-        st.session_state["array"], move_directions[randrange(4)], "B"
-    ).copy()
-    # move monster
-    st.session_state["array"] = move(
-        st.session_state["array"], move_directions[randrange(4)], "M"
-    ).copy()
-    html = level_renderer(st.session_state["array"])
+    st.session_state.right_clicked = False
     st.session_state.down_clicked = False
+
+
+if st.session_state["ending_condition"] == True:
     display_html.empty()
-    display_html = st.markdown(html, unsafe_allow_html=True)
+    if st.session_state["hero_stats"]["hp"] > 0:
+        display_html = st.markdown(
+            "You have reached to the end of the dungeon", unsafe_allow_html=True
+        )
+    if st.session_state["hero_stats"]["hp"] <= 0:
+        display_html = st.markdown("Unfortunately, you died", unsafe_allow_html=True)
 
-    #
-    # WRAP THIS PART LATER
-    #
+# ---------------- user console :-) ----------------
 
-    if Interaction("D") == True:
-        # st.write(Door())
-        st.write("door opened")
-
-    if Interaction("G") == True:
-        # st.write(Door())
-        st.write("gold acquired")
-        st.session_state["gold"] = st.session_state["gold"] + 10
-
-    # update map
-    st.session_state["array_before_move"] = st.session_state["array"]
+# if st.session_state["hero_stats"]["kills"] > 0:
+kills = "".join("💀" for i in range(st.session_state["hero_stats"]["kills"]))
+# else:
+#     kills = "0"
 
 st.caption(
-    "Level: 1" + " Gold: " + str(st.session_state["gold"]) + " Hp: 11(20) Exp: 2/14"
+    "Gold: "
+    + str(st.session_state["backpack"]["gold"])
+    + " Hp: "
+    + str(st.session_state["hero_stats"]["hp"])
+    + "(20) Exp: 2/14"
+    + " Monsters slayed: "
+    + kills
 )
 
 
-# st.markdown(
-#     """
-# <style>
-
-# </style>
-# """,
-#     unsafe_allow_html=True,
-# )
-
-
 # https://www.ee.ucl.ac.uk/~mflanaga/java/HTMLandASCIItableC1.html
+# https://htmlcolorcodes.com/color-names/
 
 with st.sidebar:
     st.write("Use keyboard arrows or buttons below")
@@ -402,7 +408,8 @@ with st.sidebar:
         st.button("DOWN", on_click=down_callback, key="DOWN")
     st.markdown("<br>", unsafe_allow_html=True)
     st.markdown(
-        '<br> <span style="color:green">@</span> - player', unsafe_allow_html=True
+        '<br> <span style="color:GreenYellow">@</span> - player',
+        unsafe_allow_html=True,
     )
     st.markdown('<br> <span style="color:red">B</span> - boss', unsafe_allow_html=True)
     st.markdown(
@@ -411,6 +418,10 @@ with st.sidebar:
     st.markdown('<br> <span style="color:gold">G</span> - gold', unsafe_allow_html=True)
     st.markdown(
         '<br> <span style="color:grey">D</span> - door',
+        unsafe_allow_html=True,
+    )
+    st.markdown(
+        '<br> <span style="color:violet">X</span> - exit',
         unsafe_allow_html=True,
     )
     st.markdown("<br>", unsafe_allow_html=True)
