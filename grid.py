c82fb522b6d4243e294b31b4f7e0587fa91dcc0f
TomJohnH <thasiow@onet.pl>
2023-01-16 22:55:10 +0100

    new engine - work in progress

--- grid.py
+++ grid.py
@@ -12,11 +12,12 @@ local_css("style.css")
 
 
 wall = "https://thumbs2.imgbox.com/10/db/7zaxbIP8_t.png"
-floor = "https://thumbs2.imgbox.com/fa/32/02bMMt5S_t.png"
+floor_plain = "https://thumbs2.imgbox.com/29/22/5rTLr6WH_t.png"
 wall_inner = "https://thumbs2.imgbox.com/a9/db/bpGqFg8q_t.png"
-wall_outer_w = "https://thumbs2.imgbox.com/ec/19/GL9KbbPq_t.png"
-wall_outer_nw = "https://thumbs2.imgbox.com/fc/b5/y7nSLPhM_t.png"
-wall_outer_n = "https://thumbs2.imgbox.com/6d/e2/7n2IlNB9_t.png"
+wall_outer_w = "https://oshi.at/ErrD/vUVT.png"
+wall_outer_nw = "https://oshi.at/VhBU/UKur.png"
+wall_outer_sw = "https://oshi.at/mJCY/VIsb.png"
+wall_outer_n = "https://oshi.at/QwVn/Lotr.png"
 floor_stain_1 = "https://thumbs2.imgbox.com/99/26/4m4rKZCS_t.png"
 npc_wizzard = "https://oshi.at/Timp/PgLb.png"
 st.markdown(
@@ -53,7 +54,22 @@ st.markdown(
         <img src="{floor_stain_1}" style="grid-column-start: 7; grid-row-start: 3;">
         <img src="{floor_stain_1}" style="grid-column-start: 8; grid-row-start: 3;">
         <img src="{floor_stain_1}" style="grid-column-start: 9; grid-row-start: 3;">
-        <img src="{floor_stain_1}" style="grid-column-start: 10; grid-row-start: 5;">
+        <img src="{floor_stain_1}" style="grid-column-start: 10; grid-row-start: 3;">
+        <img src="{wall_outer_sw}" style="grid-column-start: 1; grid-row-start: 4;">
+        <img src="{floor_plain}" style="grid-column-start: 2; grid-row-start: 4;">
+        <img src="{floor_plain}" style="grid-column-start: 3; grid-row-start: 4;">
+        <img src="{floor_plain}" style="grid-column-start: 4; grid-row-start: 4;">
+        <img src="{floor_plain}" style="grid-column-start: 5; grid-row-start: 4;">
+        <img src="{floor_plain}" style="grid-column-start: 6; grid-row-start: 4;">
+        <img src="{floor_plain}" style="grid-column-start: 7; grid-row-start: 4;">
+        <img src="{floor_plain}" style="grid-column-start: 8; grid-row-start: 4;">
+        <img src="{floor_plain}" style="grid-column-start: 9; grid-row-start: 4;">
+        <img src="{floor_plain}" style="grid-column-start: 10; grid-row-start: 4;">
+
+        <img src="{floor_plain}" style="grid-column-start: 10; grid-row-start: 5;">
+        <img src="{floor_plain}" style="grid-column-start: 10; grid-row-start: 6;">
+        <img src="{floor_plain}" style="grid-column-start: 10; grid-row-start: 7;">
+        <img src="{floor_plain}" style="grid-column-start: 10; grid-row-start: 8;">
         <img src="{npc_wizzard}" style="grid-column-start: 5; grid-row-start: 3;">
         </div>
         <div class="gamegrid2">
